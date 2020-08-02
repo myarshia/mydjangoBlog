35cb37ed0ddb025001865e8ed5960d579248e95d
myarshia <myarshia@gmail.com>
2020-08-03 01:59:06 +0430

    article_detail.html

--- articles/views.py
+++ articles/views.py
@@ -3,9 +3,11 @@ from . import models
 
 # Create your views here.
 def articles_lists(request):
-    articles = models.Article.objects.all().order_by('date')
+    articles = models.Article.objects.all().order_by('-date')
     args = {'articles':articles}
     return render(request, 'articles/articleslist.html', args)
 
 def articles_detail(request, slug):
-    return HttpResponse(slug)
+    # return HttpResponse(slug)
+    article = models.Article.objects.get(slug=slug)
+    return render(request, 'articles/article_detail.html', {'article':article})
