f9a227bd953009e6607a95393ae2661bbb12807c
myarshia <myarshia@gmail.com>
2020-08-05 23:50:02 +0430

    Login required

--- articles/urls.py
+++ articles/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "articles"
 urlpatterns = [
     path('', views.articles_lists, name="list"),
+    path('create', views.create_article, name='create'),
     path('<slug>', views.articles_detail,name="detail"),
 ]
