3f2ebca9d54284fca424d20e58df8c22b0faf737
myarshia <myarshia@gmail.com>
2020-08-07 22:09:20 +0430

    Site styling - Part 2

--- djangoBlog/urls.py
+++ djangoBlog/urls.py
@@ -19,14 +19,15 @@ from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.conf.urls.static import static
+from articles.views import articles_lists
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', views.about),
-    path('', views.home),
     path('articles/', include('articles.urls')),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
+    path('', articles_lists),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
